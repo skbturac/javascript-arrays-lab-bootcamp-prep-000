fcca1a149ef6187dd4fc92328972ab47c5d4d97e
Suheyl Turac <skbsturac@live.com>
2018-08-29 03:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,45 +2,44 @@
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function appendKitten(name){
-  return array.slice(...kittens,name)
-}
-
 function destructivelyAppendKitten(name){
-return array.slice(...kittens,name)
-}
-var kittens = ["Milo", "Otis", "Garfield"]
- function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens
 }
+
  function destructivelyPrependKitten(name){
   kittens.unshift(name)
   return kittens
 }
+
  function appendKitten(name){
   var newKittens = [...kittens]
   newKittens.push(name)
   return newKittens
 }
+
  function prependKitten(name){
   var newKittens = [...kittens]
   newKittens.unshift(name)
   return newKittens
 }
+
  function destructivelyRemoveLastKitten(){
   kittens.pop()
   return kittens
 }
+
  function destructivelyRemoveFirstKitten(){
   kittens.shift()
   return kittens
 }
+
  function removeLastKitten(){
   var newKittens = [...kittens]
   newKittens.pop()
   return newKittens
 }
+
  function removeFirstKitten(){
   var newKittens = [...kittens]
   newKittens.shift()
